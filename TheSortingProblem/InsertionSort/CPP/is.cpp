898c5e12d4a5c0550cc8424740b0ae12efa7f219
Brandon Thornburg <brandon.thornburg@avanade.com>
2021-03-31 15:23:46 -0500

    optimize

--- TheSortingProblem/InsertionSort/CPP/is.cpp
+++ TheSortingProblem/InsertionSort/CPP/is.cpp
@@ -3,47 +3,53 @@
 #include <ctime>
 #include <climits>
 #include <random>
-
 using namespace std;
 
+#define SIZE 10
+
 int random_int(int);
-void print_list(const int[]);
+void print_list(const int[], int);
+void insertion_sort(int[], int);
 
 int main(){
-    
     srand(time(NULL));
-    for (int i = 0; i < 10; i++){
-        int list[10];
-        for(int j = 0; j < 10; j++){
-            list[j] = random_int(100);
-        }
+    for (int i = 0; i < SIZE; i++){
+        
+        int list[SIZE];
+        
+        for(int &j : list)
+            j = random_int(100);
+        
         cout << "Test "<< i+1 << endl;
         cout << "Unsorted: ";
-        print_list(list);
-
-        for(int k = 1; k < 10; k++){
-            int x = list[k];
-            int y = k - 1;
-            while(y >= 0 && list[y] > x){
-                list[y + 1] = list[y];
-                y--;
-            }
-            list[y+1] = x;
-        }
+        print_list(list, SIZE);
         cout << "Sorted: ";
-        print_list(list);
+        insertion_sort(list, SIZE);
+        print_list(list, SIZE);
         cout << endl;
     }
 
     return 0;
 }
 
-void print_list(const int list[]){
-    for(int k = 0; k < 10; k++){
+void print_list(const int list[], int size){
+    for(int k = 1; k < size; k++){
         if(k != 9)
             cout << list[k] << ", ";
         else   
         cout << list[k] << endl; 
+    }
+}
+
+void insertion_sort(int list[], int size){
+        for(int k = 1; k < size; k++){
+        int x = list[k];
+        int y = k - 1;
+        while(y >= 0 && list[y] > x){
+            list[y + 1] = list[y];
+            y--;
+        }
+        list[y+1] = x;
     }
 }
 
