f0363c244835e92d7a9cde16d70c21ce3cecad4a
Brandon Thornburg <brandon.thornburg@avanade.com>
2021-03-31 17:02:40 -0500

    python

--- TheSortingProblem/InsertionSort/Python/is.py
+++ TheSortingProblem/InsertionSort/Python/is.py
@@ -1,1 +1,29 @@
+from random import *
+
+def insertion_sort(nums):
+    for x in range(len(nums)-1):
+        k = nums[x+1]
+        j = (x + 1) - 1
+        while j >= 0 and nums[j] > k:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = k
+
+def build(nums):
+    for x in range(10):
+        nums[x - 1] = randrange(100)
+
+seed(1)
+
+for x in range(10):
+    nums = []
+    for y in range(10):
+        nums.append(randint(0, 100))
+    print("Test ", x)
+    print("Unsorted: ", end='')
+    print(*nums, sep=", ")
+    insertion_sort(nums)
+    print("Sorted: ", end='')
+    print(*nums, sep=", ")
+    #print('\n')
 
